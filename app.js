eb5f963b813c407a64b24228a42e242423465ef6
PakkuDon <pakku.pd@gmail.com>
2016-05-29 23:29:36 +1000

    Input and hashed URLs now saved in MongoDB instance

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var shortHash = require('short-hash');
+var monk = require('monk');
 
 var app = express();
 app.set('port', 3000);
 
+var db = monk('localhost:27017/url-shortener');
+var urls = db.get('urls');
+
 app.get('/api/shorten/*', function(req, res) {
     var url = req.originalUrl.replace('/api/shorten/', '');
     var shortenedUrl = shortHash(url);
-    
+
+    urls.insert({
+        'original_url': url,
+        'shortened_url': shortenedUrl
+    });
     res.status(200).json({
         'original_url': url,
         'shortened_url': shortenedUrl
