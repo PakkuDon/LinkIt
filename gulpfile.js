390a29f8a1967e616474d566f3c14d218648c210
PakkuDon <pakku.pd@gmail.com>
2017-02-05 23:50:41 +1100

    Remove Bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,14 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('vendor', function() {
-    gulp.src([
-        'bower_components/jquery/dist/jquery.min.js'
-    ])
-        .pipe(uglify())
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('bundle', function() {
+gulp.task('minify-client', function() {
     gulp.src('client.js')
         .pipe(uglify())
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('client', function() {
-    gulp.src('client.js')
-        .pipe(uglify())
-        .pipe(concat('client.js'))
-        .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('styles', function() {
-    gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
-        .pipe(concat('style.css'))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('default', ['vendor', 'bundle', 'client', 'styles']);
+gulp.task('default', ['minify-client']);
 gulp.task('watch', function() {
-  gulp.watch('client.js', ['client']);
+  gulp.watch('client.js', ['minify-client']);
 });
